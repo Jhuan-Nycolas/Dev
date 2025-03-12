9c0fa0d2db0e84ed7cd3dea5a407b76d22810c1a
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-12 11:40:00 -0300

    Criando um DevShell pra codar um portifólio usando vue

---
+++ Vue/Test/flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Nix Yarn DevShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = [ pkgs.yarn ];
+    };
+  };
+}
