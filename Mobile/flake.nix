b0b09d99873f8715a7fc5662e25ab7c6a45735e2
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-03-21 11:50:40 -0300

    Backup

---
+++ Mobile/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Nix Native DevShell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.android-tools
+          pkgs.yarn
+        ];
+
+        shellHook = "clear; exec fish";
+      };
+    };
+  };
+}
