61055fa758e82e9c29936d6359f4bb7c771b6843
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-04-03 12:09:20 -0300

    Criando uma pasta para rust

--- Python/open.py
+++
@@ -1,8 +0,0 @@
-import gets
-from json import load
-
-with open("config.json") as config:
-  global conf
-  conf = load(config)
-
-editor = gets.getEditor(conf)
---
+++ Rust/target/debug/Rust.d
@@ -0,0 +1,1 @@
+/home/tux/Dev/Rust/target/debug/Rust: /home/tux/Dev/Rust/src/main.rs
