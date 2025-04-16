7ae2d9613895f140f335bf16297a4aaa7ca4e29c
Jhuan-Nycolas <jhuan2024@hotmail.com>
2025-04-15 23:37:32 -0300

    Aprendendo haskell

--- Haskell/program.hs
+++ Haskell/program.hs
@@ -1,3 +1,10 @@
 main :: IO ()
+nome :: String -> String
+nome nm =
+  if nm == "Jhuan" || nm == "jhuan"
+    then "Como vai as aulas de Haskell?"
+    else "Oi " ++ nm
 main = do
-  putStrLn "Hi"
+  putStrLn "Qual seu nome?"
+  name <- getLine
+  putStrLn (nome name)
